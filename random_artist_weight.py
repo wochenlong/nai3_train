0168aafc29c11932eb6902aa2e83a708877b17f7
windsing <117965575+wochenlong@users.noreply.github.com>
2024-01-31 03:45:10 +0800

    随机权重组合

--- random_artist_weight.py
+++ random_artist_weight.py
@@ -1,37 +1,3 @@
-from itertools import product
-
-# 定义艺术家和括号的列表
-artists = [
-    "artistA",
-    "artistB",
-    "artistC",
-    "artistD",
-    "artistE",
-    "artistF",
-    "artistG",
-    "artistH",
-    "artistI",
-    "artistJ",
-]
-brackets = ["[]", "{}"]
-
-# 创建一个空列表用于存储所有可能的组合
-combinations = []
-
-# 根据艺术家数量生成所有可能的括号组合
-for combo in product(brackets + [""], repeat=len(artists)):
-    combination = ", ".join(
-        [f"{b[0]}{a}{b[1]}" if b != "" else a for a, b in zip(artists, combo)]
-    )
-    combinations.append(combination)
-
-# 打印所有可能的组合
-print("所有可能的组合：")
-for combination in combinations:
-    print(combination)
-
-# 打印总共生成的组合数量
-print("总共有", len(combinations), "种组合。")
 import random
 from itertools import product
 
