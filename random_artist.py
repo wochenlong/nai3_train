a5798d666ce68d6a9fcd69e8b530000dc9a88e5d
zhulinyv <zhulinyv2005@outlook.com>
2024-03-20 21:21:30 +0800

    :fire: 删除无效代码

--- random_artist.py
+++ random_artist.py
@@ -5,6 +5,4 @@ from utils.utils import *
 # 创建 NovelaiImageGenerator 实例
 generator = NovelaiImageGenerator(mode="1", prompt_folder=env_vars["prompt_folder"], characters_path=env_vars["characters_path"])
 
-str_artist = get_random_artist()
-
 generate(generator)
--- random_characetrs.py
+++ random_characetrs.py
@@ -4,12 +4,4 @@ from utils.utils import *
 
 generator = NovelaiImageGenerator(mode="2", prompt_folder=env_vars["prompt_folder"], characters_path=env_vars["characters_path"])
 
-# 加载角色列表
-generator.load_characters()
-
-# 生成并保存图像
-image_data = generator.generate_image(env_vars["prefix"])
-
-save_image_from_binary(image_data, env_vars["folder_path"])
-
 generate(generator)
