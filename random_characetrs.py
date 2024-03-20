140bfea30b3094cdb9516b2308a5c4ed145df730
zhulinyv <zhulinyv2005@outlook.com>
2024-03-20 22:22:39 +0800

    :bug: 删多了

--- random_characetrs.py
+++ random_characetrs.py
@@ -4,4 +4,7 @@ from utils.utils import *
 
 generator = NovelaiImageGenerator(mode="2", prompt_folder=env_vars["prompt_folder"], characters_path=env_vars["characters_path"])
 
+# 加载角色列表
+generator.load_characters()
+
 generate(generator)
