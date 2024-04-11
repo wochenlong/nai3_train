ebd3d751338995fa17c6a098e1894ea2154d40fe
sudoskys <coldlando@hotmail.com>
2024-04-11 10:33:13 +0800

    :recycle: refactor(run_cut): refactor image processing logic in run_cut.py
    调用重置透明度摧毁隐水印

--- run_cut.py
+++ run_cut.py
@@ -1,44 +1,23 @@
-from PIL import Image
 import os
-from tqdm import tqdm
+from io import BytesIO
+
+from novelai_python.tool.image_metadata import ImageMetadata
 
 # 源图片路径
 source_path = input("请输入源图片路径:")
-
 # 新文件夹路径
 output_path = input("请输入新文件夹路径:")
-
 # 创建新文件夹
 os.makedirs(output_path, exist_ok=True)
-
 # 获取源图片路径下的所有文件
 files = [f for f in os.listdir(source_path) if f.endswith(".png")]
 
-# 进度条
-progress_bar = tqdm(total=len(files), desc="Processing images")
-
 # 遍历源图片路径下的所有文件
 for filename in files:
-    # 加载图片
-    image_path = os.path.join(source_path, filename)
-    image = Image.open(image_path)
-
-    # 裁剪底部100像素
-    cropped_image = image.crop((0, 0, 832, 1116))
-
-    # 生成新文件路径
-    output_file = os.path.join(output_path, filename)
-
-    # 保存裁剪后的图片到新文件夹
-    cropped_image.save(output_file)
-
-    # 关闭图片
-    image.close()
-    cropped_image.close()
-
-    # 更新进度条
-    progress_bar.update(1)
-    progress_bar.set_postfix({"Remaining": len(files) - progress_bar.n})
-
-# 关闭进度条
-progress_bar.close()
+    # 读取文件后调用 ImageMetadata.reset_alpha() 方法，然后写回文件
+    with open(os.path.join(source_path, filename), "rb") as f:
+        reset = ImageMetadata.reset_alpha(BytesIO(f.read()))
+        output_file = os.path.join(output_path, filename)
+        # 保存裁剪后的图片到新文件夹
+        with open(output_file, "wb") as out:
+            out.write(reset.getvalue())
